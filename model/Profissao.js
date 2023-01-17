3bbf7218c499b8bce44ebc9448adb8dabe79ccfa
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-17 20:15:28 -0300

    criando model profissao

---
+++ model/Profissao.js
@@ -0,0 +1,15 @@
+const { INTEGER } = require('sequelize');
+const sequelize = require('../db/conn');
+const {DataTypes} = require('sequelize');
+
+const Profissoes = sequelize.define('profissao', {
+    id_profissao: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    descricao: {type: DataTypes.STRING, allowNull: false}
+});
+
+module.exports = Profissoes;
