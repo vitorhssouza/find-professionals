232ad6521d25da6a237dc37cee40e916f31c5c34
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-17 20:15:19 -0300

    criando model prestadores

---
+++ model/Prestadores.js
@@ -0,0 +1,24 @@
+const { INTEGER } = require('sequelize');
+const sequelize = require('../db/conn');
+const {DataTypes} = require('sequelize');
+const Endereco = require('../model/Endereco');
+
+const Prestadores = sequelize.define('prestadores', {
+    id_prestadores: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    nome: {type: DataTypes.STRING(100), allowNull: false},
+    sobrenome: {type: DataTypes.STRING(100), allowNull: false},
+    cpf_cnpj: {type: DataTypes.STRING(16), allowNull: false},
+    contato: {type: DataTypes.STRING(45), allowNull: false},
+    email: {type: DataTypes.STRING(70), allowNull: false},
+    senha: {type: DataTypes.STRING(500), allowNull: false},
+});
+
+Prestadores.belongsTo(Endereco, {foreignKey: 'id_endereco'})
+
+module.exports = Prestadores;
+
