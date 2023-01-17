898ac7df226f659e7e6961ba1cebc7cdf785ffd6
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-17 20:15:36 -0300

    criando model serviços

---
+++ model/Servicos.js
@@ -0,0 +1,25 @@
+const { INTEGER } = require('sequelize');
+const sequelize = require('../db/conn');
+const {DataTypes} = require('sequelize');
+const Clientes = require('../model/Clientes');
+const Prestadores = require('../model/Prestadores');
+
+
+const Servicos = sequelize.define('servicos', {
+    id_servico: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    descricao: {type: DataTypes.STRING(300), allowNull: false}
+});
+
+
+Prestadores.hasMany(Servicos, {foreignKey: 'id_prestadores'});
+Servicos.belongsTo(Prestadores, {foreignKey: 'id_prestadores'});
+
+Clientes.hasMany(Servicos, {foreignKey: 'id_clientes'});
+Servicos.belongsTo(Clientes, {foreignKey: 'id_clientes'});
+
+module.exports = Servicos;
