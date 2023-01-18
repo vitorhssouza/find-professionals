502d34bd8e520931a4cedbb0214286941fdc7268
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-18 20:41:12 -0300

    começando a criar metodos para cadastrar e verificar login

--- controller/HomeControlllers.js
+++ controller/HomeControlllers.js
@@ -5,6 +5,41 @@ class HomeController{
         res.render('home')
     }
 
+    static login(req, res){
+        res.render('login')
+    }
+
+    static cadastro(req, res){
+        res.render('cadastro')
+    }
+
+    static cadastroSave(req, res){
+
+        //let nome = req.body.nome;
+        //const sobrenome = req.body.nome;
+        // const cpf = req.body.cpf;
+        // const contato = req.body.contato;
+        // const endereco = req.body.endereco;
+        // const numero = req.body.numero;
+        // const bairro = req.body.bairro;
+        // const cidade = req.body.cidade;
+        // const cep = req.body.cep;
+        // const estado = req.body.estado;
+        // const usuario = req.body.usuario;
+        // const email = req.body.email;
+        // const senha = req.body.senha;
+        // const confirmaSenha = req.body.senhaConfirme
+
+        console.log(nome)
+
+
+
+    }
+
+    static loginVerificar(req, res){
+        const nome = req.body.email
+        console.log(nome)
+    }
 
 
 }
