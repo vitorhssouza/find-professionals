5d5232cd3d6c04e1d29cc9f8204480c48b8e6053
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-18 15:34:54 -0300

    chamando a rota principal e colocando os parametro para autenticacao

--- index.js
+++ index.js
@@ -15,15 +15,55 @@ const Servicos = require('./model/Servicos')
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
-
 // Importando metodo flash
 const flash = require('express-flash');
-
-
 
 //Define o Handlebars como Template Engine da nossa aplicação
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+
+// Parametro de autenticação 
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso-secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+    cookie: {
+        secure: false,
+        maxAge: 360000,
+        expires: new Date(Date.now(), + 360000),
+        httpOnly: true
+    }
+    })
+);
+
+
+// Importando a rota principal 
+const menu = require('./routers/homeRoutes');
+app.use(menu);
+
+
+
+//Seta sessões para requisição 
+app.use((req, res, next) => {
+    if(req.session.userId){
+        res.locals.session = req.session
+    }
+    next()
+});
+
+
+
+// configurando flash messagens
+app.use(flash());
+
+
+
 
 //Configuração de formulario para receber os dados
 app.use(express.urlencoded({extended: true}));
@@ -33,7 +73,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //Verificando conexão com o base de dados e executando o servidor
-conn.sync({force: true}).then(() => {
+conn.sync().then(() => {
     app.listen(3000, () => {
         console.log("Servidor Executando em localhost:3000");
     });
