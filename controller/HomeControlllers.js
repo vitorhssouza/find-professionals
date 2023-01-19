4b260633b4e16ea7fdd830eb425deaec717b694f
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-19 15:13:40 -0300

    criando verificações e salvando no banco os usuarios cadastrados

--- controller/HomeControlllers.js
+++ controller/HomeControlllers.js
@@ -1,3 +1,7 @@
+const Endereco = require('../model/Endereco')
+const Prestadores = require('../model/Prestadores')
+const Clientes = require('../model/Clientes')
+
 const bcrypt = require('bcryptjs');
 
 class HomeController{
@@ -13,27 +17,65 @@ class HomeController{
         res.render('cadastro')
     }
 
-    static cadastroSave(req, res){
+    static async cadastroSave(req, res){
 
-        //let nome = req.body.nome;
-        //const sobrenome = req.body.nome;
-        // const cpf = req.body.cpf;
-        // const contato = req.body.contato;
-        // const endereco = req.body.endereco;
-        // const numero = req.body.numero;
-        // const bairro = req.body.bairro;
-        // const cidade = req.body.cidade;
-        // const cep = req.body.cep;
-        // const estado = req.body.estado;
-        // const usuario = req.body.usuario;
-        // const email = req.body.email;
-        // const senha = req.body.senha;
-        // const confirmaSenha = req.body.senhaConfirme
+        let nome = req.body.nome;
+        const sobrenome = req.body.nome;
+        const cpf = req.body.cpf;
+        const contato = req.body.contato;
+        const logadouro = req.body.endereco;
+        const numero = req.body.numero;
+        const bairro = req.body.bairro;
+        const cidade = req.body.cidade;
+        const cep = req.body.cep;
+        const estado = req.body.estado;
+        const usuario = req.body.usuario;
+        const email = req.body.email;
+        const senha = req.body.senha;
+        const confirmaSenha = req.body.senhaConfirme
 
-        console.log(req.body)
+        const userCliente = await Clientes.findOne({where: {email: email, cpf: cpf}})
+        const userPrestador = await Prestadores.findOne({where: {email: email, cpf_cnpj: cpf}})
 
+        // Verificação de senha 
+        if(senha != confirmaSenha){
+            console.log('Senha incorretas!')
+            res.redirect('/cadastro');
+        }else if(userCliente){
+            console.log('Esse cliente já existe')
+            res.redirect('/login')
+        }else if(userPrestador){
+            console.log('Esse prestador já existe')
+            res.redirect('/login')
+        }else{
+            // Salvando os dados no banco
+            const teste =  await Endereco.create({
+                logadouro, numero, bairro, cidade, cep, estado
+            })
 
+            const endereco = await Endereco.findOne({raw: true, where:{id_endereco: teste.id_endereco}})
 
+            const salt = bcrypt.genSaltSync(10);
+            const senhaCriptografada = bcrypt.hashSync(senha, salt);
+
+            if (usuario == 'Cliente'){
+                await Clientes.create({
+                    nome, sobrenome, cpf, contato, email, senha: senhaCriptografada, id_endereco: endereco.id_endereco
+                });
+                console.log('Cadastrado')
+                res.redirect('/login')
+            }else{
+                await Prestadores.create({
+                    nome, sobrenome, cpf_cnpj: cpf, contato, email, senha: senhaCriptografada, id_endereco: endereco.id_endereco
+                });
+                console.log('Cadastrado')
+                res.redirect('/login')
+                
+            }
+        };
+
+        
+        
     }
 
     static loginVerificar(req, res){
