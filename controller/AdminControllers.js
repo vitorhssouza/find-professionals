a82bdc563c57134549342253626309e8b76c3016
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-21 09:45:02 -0300

    criando metodos de visualização de prestadores

--- controller/AdminControllers.js
+++ controller/AdminControllers.js
@@ -1,7 +1,27 @@
+const Clientes = require("../model/Clientes")
+const Endereco = require("../model/Endereco")
+
 class AdminControllers{
+
+    // Metodo home admin
     static adminHome(req, res){
-        res.render('homeAdmin')
+        res.render('admin/homeAdmin', {layout: false})
     }
+
+    // Metodo que lista todos os clientes para admin
+    static async clientes(req, res){
+
+        const clientes = await Clientes.findAll({
+            raw: true,
+            include: {
+                model: Endereco
+            }
+        })
+
+        res.render('admin/clientes', {layout: false, clientes})
+    }
+
+
 }
 
 
