d44f4216a3a466f9a195b7549dcc27fa54b9475d
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-21 09:24:29 -0300

    criando rotas para prestadores e clientes

--- routers/adminRoutes.js
+++ routers/adminRoutes.js
@@ -3,7 +3,14 @@ const express = require('express');
 const router = express.Router();
 const AdminControllers = require('../controller/AdminControllers')
 
+// Rota do menu admin
 router.get('/adminhome', AdminControllers.adminHome)
+
+// Rota do menu administrador que listra todos os cliente
+router.get('/admin/clientes', AdminControllers.clientes)
+
+// Rota do menu admintrador que listra todos os prestadores
+router.get('/admin/prestadores', AdminControllers.prestadores)
 
 
 module.exports = router
