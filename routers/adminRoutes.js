4a7102d962563754ba3a7350d56768f7fa3e345d
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-21 14:55:27 -0300

    criado rotas de detalhes de prestadores e de listar prestadores

--- routers/adminRoutes.js
+++ routers/adminRoutes.js
@@ -9,10 +9,13 @@ router.get('/adminhome', AdminControllers.adminHome)
 // Rota do menu administrador que listra todos os cliente
 router.get('/admin/clientes', AdminControllers.clientes)
 
+// Rota de detalhe de cliente
 router.get('/admin/detalhes/clientes/:id_clientes', AdminControllers.detalhes)
 
 // Rota do menu admintrador que listra todos os prestadores
 router.get('/admin/prestadores', AdminControllers.prestadores)
 
+// Rota de detalhes de prestador
+router.get('/admin/detalhes/prestador/:id_prestadores', AdminControllers.detalhesPrestadores)
 
 module.exports = router
