93c3189723f7de41b6ffaee9a431335ef885f9e8
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-23 17:26:02 -0300

    criando metodo que mostra uma tabela com todos os prestadores na tela inicial cliente

--- controller/ClienteControllers.js
+++ controller/ClienteControllers.js
@@ -1,10 +1,17 @@
 const Clientes = require("../model/Clientes")
+const Prestadores = require('../model/Prestadores')
+const Profissoes = require("../model/Profissoes")
 
 class ClientesControllers{
     static async clienteHome(req, res){
         const id_clientes = req.session.userIdCliente
         const cliente = await Clientes.findOne({raw: true, where:{id_clientes: id_clientes}})
-        res.render('clientes/homeClientes', {layout: false, cliente})
+        const prestadores = await Profissoes.findAll({
+            raw: true,
+            include: {model: Prestadores}
+        });      
+
+        res.render('clientes/homeClientes', {layout: false, cliente, prestadores})
     }
 }
 
