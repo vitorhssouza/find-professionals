10ea710061c2eca1aadc5ff7339aea2e1956d44b
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-23 18:08:05 -0300

    criando rota de perfil

--- routers/clientesRoutes.js
+++ routers/clientesRoutes.js
@@ -6,4 +6,7 @@ const ClienteControllers = require('../controller/ClienteControllers')
 // Rota menu cliente
 router.get('/homeCliente', ClienteControllers.clienteHome)
 
+// Rota de perfil cliente
+router.get('/homeCliente/perfil', ClienteControllers.perfil)
+
 module.exports = router
