9ab1dbaf28a08ffa68a6a1cb0c7b2189b59d5ee0
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-23 18:46:40 -0300

    criando metodo de edição de perfil cliente

--- controller/ClienteControllers.js
+++ controller/ClienteControllers.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize")
 const Clientes = require("../model/Clientes")
 const Endereco = require("../model/Endereco")
 const Prestadores = require('../model/Prestadores')
@@ -15,6 +16,7 @@ class ClientesControllers{
         res.render('clientes/homeClientes', {layout: false, cliente, prestadores})
     };
 
+    // Método para mostrar perfil
     static async perfil(req, res){
         const id_clientes = req.session.userIdCliente;
         const cliente = await Clientes.findOne({
@@ -24,7 +26,52 @@ class ClientesControllers{
         });
 
         res.render('clientes/perfil', {layout: false, cliente})
-    }
+    };
+
+    // Metodo para editar clientes
+    static async edit(req, res){{
+        const id = req.session.userIdCliente
+        const cliente = await Clientes.findOne({
+            raw: true,
+            include: {model: Endereco},
+            where:{id_clientes: id}
+        });
+        res.render('clientes/edit', {layout: false, cliente})
+    }};
+
+    // Método que salvar a edição de clientes
+    static async editSave(req, res){
+        const id = req.session.userIdCliente;
+        const nome = req.body.nome;
+        const sobrenome = req.body.sobrenome;
+        const cpf = req.body.cpf;
+        const contato = req.body.contato;
+        const logadouro = req.body.endereco;
+        const numero = req.body.numero;
+        const bairro = req.body.bairro;
+        const cidade = req.body.cidade;
+        const cep = req.body.cep;
+        const estado = req.body.estado;
+
+        const cliente = await Clientes.findOne({raw: true, where: {id_clientes: id}})
+
+        try{
+            const editEndereco = await Endereco.update(
+                {logadouro, numero, bairro, cidade, cep, estado},
+                {where: {id_endereco: cliente.id_endereco}}
+                );
+            
+            const editCliente = await Clientes.update(
+                {nome, sobrenome, cpf, contato},
+                {where: {id_clientes: id}}
+                );
+            res.redirect('/homeCliente/perfil')
+        }catch (error){
+            console.log(error)
+        }     
+
+    };
+
 }
 
 module.exports = ClientesControllers
