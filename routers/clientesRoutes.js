6733aa0abf8a373ff3b641091d7e3a6f7629dae2
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-23 19:36:09 -0300

    fazendo verificação de sessões

--- routers/clientesRoutes.js
+++ routers/clientesRoutes.js
@@ -2,17 +2,18 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const ClienteControllers = require('../controller/ClienteControllers')
+const { checarLogadoCliente } = require('../helper/authLoginCliente')
 
 // Rota menu cliente
-router.get('/homeCliente', ClienteControllers.clienteHome)
+router.get('/homeCliente', checarLogadoCliente, ClienteControllers.clienteHome)
 
 // Rota de perfil cliente
-router.get('/homeCliente/perfil', ClienteControllers.perfil)
+router.get('/homeCliente/perfil', checarLogadoCliente, ClienteControllers.perfil)
 
 // Rota de editar perfil
-router.get('/homeCliente/perfil/edit', ClienteControllers.edit)
+router.get('/homeCliente/perfil/edit', checarLogadoCliente, ClienteControllers.edit)
 
 // Rota de salvar edição no perfil
-router.post('/homeCliente/perfil/editSave', ClienteControllers.editSave)
+router.post('/homeCliente/perfil/editSave', checarLogadoCliente, ClienteControllers.editSave)
 
 module.exports = router
