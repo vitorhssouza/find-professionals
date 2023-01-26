1133953b201495cadc292182cd193a0955805f91
Vitor Souza <vitorugo_kta@hotmail.com>
2023-01-26 20:30:25 -0300

    criando rotas de detalhes e serviço

--- routers/prestadoresRoutes.js
+++ routers/prestadoresRoutes.js
@@ -7,6 +7,12 @@ const { checarLogadoPrestador } = require('../helper/authLoginPrestador')
 // Rota de menu principal prestadores
 router.get('/homePrestador', checarLogadoPrestador, PrestadorControllers.home)
 
+// Rota de detalhes de clientes
+router.get('/prestador/detalhes/cliente/:id_clientes/:id_servico', checarLogadoPrestador, PrestadorControllers.detalhes);
+
+// Rota de aceitar servico
+router.get('/prestador/aceitar/cliente/:id_servico', checarLogadoPrestador, PrestadorControllers.aceitar);
+
 
 
 module.exports = router;
